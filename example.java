15c0aeb30b6e7df81d6ed5a03a4e6818c85148f2
Gloria192 <mutuyegloria@gmail.com>
2025-01-29 16:33:30 -0800

    six committ

--- example.java
+++ example.java
@@ -9,8 +9,17 @@
     //     int result=num1+num2;
     // System.out.print(result);
     // declare byte
-    byte b = 125;
-    int c= b;
-    System.out.println(c);
+    // byte b = 125;
+    // int c= b;
+    // System.out.println(c);
+    // it will perfom the modulus and get remainder 257%256
+    // int c=257;
+    // byte k=(byte)c;
+    // System.out.println(k);
+    // type promation
+    byte b=10;
+    byte c=30;
+    int result=b*c;
+    System.out.println(result);
    }
 }
