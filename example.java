aa662679e4af46375856ba226ca2f97e4959b7a1
Gloria192 <mutuyegloria@gmail.com>
2025-01-29 16:40:36 -0800

    seven commit

--- example.java
+++ example.java
@@ -3,23 +3,36 @@
 
    public static void main(String[] a)
     {
-        // additional
+        // 1.additional
     //     int num1=0x1;
     //     int num2=5;
     //     int result=num1+num2;
     // System.out.print(result);
-    // declare byte
+    //2. declare byte
     // byte b = 125;
     // int c= b;
     // System.out.println(c);
-    // it will perfom the modulus and get remainder 257%256
+    // 3.it will perfom the modulus and get remainder 257%256
     // int c=257;
     // byte k=(byte)c;
     // System.out.println(k);
-    // type promation
-    byte b=10;
-    byte c=30;
-    int result=b*c;
-    System.out.println(result);
+    // 4.type promation
+    // byte b=10;
+    // byte c=30;
+    // int result=b*c;
+    // System.out.println(result);
+    // 5.operators
+    int num1=8;
+    int num2=5;
+    int result1=num1+num2;
+    int result2=num1-num2;
+    int result3=num1*num2;
+    int result4=num1/num2;
+    int result5=num1%num2;
+    System.out.println(result1);
+    System.out.println(result2);
+    System.out.println(result3);
+    System.out.println(result4);
+    System.out.println(result5);
    }
 }
