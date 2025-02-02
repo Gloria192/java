e2556f4cb3d4778031a2d9dbfced3d64737a8e7f
Gloria192 <mutuyegloria@gmail.com>
2025-02-02 01:55:23 -0800

    13 commit

--- example.java
+++ example.java
@@ -96,8 +96,17 @@
 //    y++;
 // }while(y<=5);
 //for loop
-for(int i=1;i<=4;i++){
-   System.out.println(i);
-}
+// for(int i=1;i<=4;i++){
+//    System.out.println(i);
+// }
+// print number and days
+for(int i=1;i<=7;i++){
+   System.out.println("Day"+i);
+   
+   for(int j=1;j<=9;j++){
+      System.out.println(""+(j+8) + " -" +(j+9));
+      
    }
+}
+    }
 }
