66d93bae8665a121f43adad8ffc677362bf7c565
Gloria <mutuyegloria@gmail.com>
2025-03-21 04:51:52 -0700

    adding some changes

--- example.java
+++ example.java
@@ -103,7 +103,7 @@
 for(int i=1;i<=7;i++){
    System.out.println("Day"+i);
    
-   for(int j=1;j<=9;j++){
+   for(int j=0;j<=10;j++){
       System.out.println(""+(j+8) + " -" +(j+9));
       
    }
